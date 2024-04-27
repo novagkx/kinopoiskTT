638d265bafb886f1476408beb92bfdb11bc44032
Artyom Novikov <lemerl12@mail.ru>
2024-04-27 14:08:30 +0300

    implement filmcard by modal

--- vkTT/src/components/FilmItem.jsx
+++ vkTT/src/components/FilmItem.jsx
@@ -1,14 +1,11 @@
-import FilmService from "../API/FilmService.js";
-export default function FilmItem ({blockName, film}) {
-    // const ShowFilm = (id) => {
-    //     // const filmData = FilmService.getFilm(1,1,id);
-        
-    //     //логика отображения фильма (через модалку)
-    // } поместить этот обработчик в ссылку section__link -> onClick={()=>{ShowFilm(film.id)}}
+// import FilmService from "../API/FilmService.js";
+
+export default function FilmItem ({blockName, film, onclick}) {
+    
     return (
         <li className={blockName + "__item"} key={film.id}>
-                    <a className='section__link' >
-                    <img style={{maxWidth: "100%", maxHeight:"100%"}} src={film.poster.previewUrl
+                    <a className='section__link' onClick={onclick}>
+                    <img style={{maxWidth: "100%", maxHeight: "100%"}} src={film.poster.previewUrl
 }></img>
                     <div className="section__items-container">
                         <h3 className="section__rating">{(film.rating.kp).toFixed(1)}</h3>
